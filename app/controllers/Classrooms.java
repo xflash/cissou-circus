a3e1b1b5f994b1b675f52091ead22aeba8781f8c
Rémy Coqueugniot <remy.coqueugniot@skyguide.ch>
2019-03-15 08:55:18 +0100

    Change classrooms selection based on its kind

--- app/controllers/Classrooms.java
+++ app/controllers/Classrooms.java
@@ -12,10 +12,15 @@ import play.mvc.Controller;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import static java.util.Arrays.*;
 
 /**
  * @author rcoqueugniot
@@ -32,7 +37,13 @@ public class Classrooms extends Controller {
         if (classrooms.isEmpty())
             init();
 
-        List<Long> selected = Classroom.find("select c.id from Classroom c where c.name like 'GRANDE%'").fetch();
+        List<Long> selected =
+                Classroom.find("select c.id from Classroom c where c.kind in :kinds ")
+                        .bind("kinds",
+                                stream(ClassRoomKind.values())
+                                        .filter(value -> value.ordinal() >= ClassRoomKind.GRANDE_MOYENNE_SECTION.ordinal())
+                                        .collect(Collectors.toList()))
+                        .fetch();
         render(classrooms, selected);
     }
 
